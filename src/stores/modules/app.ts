6cb64cefc63d74808075a884b269bbd528c7317e
bear320 <aaabear320@gmail.com>
2023-06-24 01:06:30 +0800

    feat: 🎸 新增切換深色主題功能

--- src/stores/modules/app.ts
+++ src/stores/modules/app.ts
@@ -12,7 +12,22 @@ export const useAppStore = defineStore('app', () => {
   const currentTab = ref<Tab>(Tab.about);
   const isDarkMode = ref<boolean>(false);
   function toggleTheme() {
+    // 透過 localStorage 獲取先前主題
+    const previousTheme = localStorage.getItem('isDarkMode');
+
+    // 如果 localStorage 有相關紀錄，則依該紀錄設定 isDarkMode
+    if (previousTheme !== null) {
+      isDarkMode.value = previousTheme === 'true' ? true : false;
+    }
+
+    // 切換模式
     isDarkMode.value = !isDarkMode.value;
+
+    // 操作 body 之 class
+    document.body.classList.toggle('dark-theme');
+
+    // 將目前主題儲存至 localStorage
+    localStorage.setItem('isDarkMode', isDarkMode.value === true ? 'true' : 'false');
   }
   return { currentTab, isDarkMode, toggleTheme };
 });
